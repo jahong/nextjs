fd9eb250008c5af89a068c80ec5d34843cfb989c
hwan-cheol  yong <21moru@gmail.com>
2021-10-05 14:39:42 +0900

    수정

---
+++ next-sg/pages/factory/factoryList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const factoryList = () => {
+  const router = useRouter();
+  console.log(router);
+
+  return (
+    <div className={"container"}>
+        <Link href="/">Move to '/’</Link>
+    </div>
+  );
+};
+
+export default factoryList;
